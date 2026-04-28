75c6a83b3f69ccbd9b1118f98fb9727828dee18c
LeonovAndreww <andrewleonov2007@gmail.com>
2026-04-28 17:52:07 -0500

    Implemented preprocess

--- parser.py
+++ parser.py
@@ -62,7 +62,12 @@ def preprocess(sentence):
     and removing any word that does not contain at least one alphabetic
     character.
     """
-    raise NotImplementedError
+    for i in range (10):
+        sentence.replace(str(i), "")
+    sentence = sentence.lower().replace(".", "").replace(",","")
+    words = nltk.word_tokenize(sentence)
+    print(words)
+    return words
 
 
 def np_chunk(tree):
